5ea0ebf92b60d5d9d2411dd080ccfb01eb6254ac
elli karg <elisa.karg.bogor@gmail.com>
2023-09-18 06:24:13 +0000

    feat: add room model

--- hostel/models.py
+++ hostel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.id} room {self.name} with {self.beds} beds for {self.capacity} persons"
