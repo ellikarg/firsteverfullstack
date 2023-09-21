503f5f1cb542e47ec3d990678cc6bfd8baee3ce2
elli karg <elisa.karg.bogor@gmail.com>
2023-09-21 18:47:08 +0000

    feat: adjust responsiveness on html templates

--- hostel/urls.py
+++ hostel/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('room_list/', views.RoomListView, name='room_list'),
     path('booking_list/', BookingList.as_view(), name='booking_list'),
     path('book/', BookingView.as_view(), name='booking_view'),
-    path('room/<str:name>/', RoomDetailView.as_view(), name='room_detail_view'),
+    path('room/<str:name>/', RoomDetailView.as_view(), name='room_detail_view')
 ]
