a7ea0155071c7c226e994ba73d0705aa00fa9bfc
elli karg <elisa.karg.bogor@gmail.com>
2023-09-22 20:10:02 +0000

    fix: add '*' to allowed hosts

--- firsteverfullstack/settings.py
+++ firsteverfullstack/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'firsteverfullstack-7b5d63bca638.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'firsteverfullstack-7b5d63bca638.herokuapp.com', '*']
 
 
 # Application definition
